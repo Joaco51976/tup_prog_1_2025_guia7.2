a09819556173399859354e63196a6d61a0cdb94b
Joaco51976 <joaquincordini62@gmail.com>
2025-06-02 16:26:04 -0300

    Add files via upload

---
+++ Positvo - Negativo - Cero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace posnegcer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            double numero = Convert.ToDouble(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                Console.WriteLine("El numero ingresado es positivo");
+            }
+            else if (numero < 0)
+            {
+                Console.WriteLine("El numero ingresado es negativo");
+            }
+            else
+            {
+                Console.WriteLine("El numero ingresado es cero");
+            }
+        }
+    }
+}
