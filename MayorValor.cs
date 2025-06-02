6c7e76c7b8d712d4c008768679510c441b3fde05
Joaco51976 <joaquincordini62@gmail.com>
2025-06-02 18:52:41 -0300

    Add files via upload

---
+++ MayorValor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MayorValor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, mayor;
+
+            Console.Write("Ingrese un número: ");
+            num = int.Parse(Console.ReadLine());
+            mayor = num;
+
+            for (int i=1; i<6; i++)
+            {
+                Console.Write("Ingrese otro número: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num > mayor)
+                {
+                    mayor = num;
+                }
+            }
+
+            Console.WriteLine($"El número mayor es: {mayor}");
+        }
+    }
+}
