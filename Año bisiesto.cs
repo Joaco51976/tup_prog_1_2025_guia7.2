1bf707871a0fdd662168c9c0ea9466d49746649d
Joaco51976 <joaquincordini62@gmail.com>
2025-07-16 12:16:33 -0300

    Add files via upload

---
+++ Año bisiesto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un año");
+            int año = int.Parse(Console.ReadLine());
+
+            if (año % 4 == 0 && (año % 100 != 0 || año % 400 == 0))
+            {
+                Console.WriteLine("El año ingresado es bisiesto.");
+            }
+            else
+            {
+                Console.WriteLine("El año ingresado no es bisiesto.");
+            }
+        }
+    }
+}
